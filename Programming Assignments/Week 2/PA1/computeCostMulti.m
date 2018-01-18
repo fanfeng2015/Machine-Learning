5a2faf61715dd26eadff30dc930a288a24eb3df4
Fan Feng <fanfeng2015@gmail.com>
2018-01-17 21:20:02 -0800

    Week 2 Modification

--- Programming Assignments/Week 2/PA1/computeCostMulti.m
+++ Programming Assignments/Week 2/PA1/computeCostMulti.m
@@ -1,6 +1,7 @@
 function J = computeCostMulti(X, y, theta)
 % COMPUTECOSTMULTI Computes cost for linear regression with multiple
 % variables.
+
 % J = COMPUTECOSTMULTI(X, y, theta) computes the cost of using theta as the
 % parameter for linear regression to fit the data points in X and y
 
